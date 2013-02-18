593178d8512922b41e3b95967911b67cf883d1b9
medici <the.medici.florentine@gmail.com>
2013-02-18 16:31:39 +0800

    add static-pages contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 	
   describe "Home page" do
  		
@@ -12,7 +14,7 @@ describe "StaticPages" do
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
     	page.should have_selector('title',
-    		                        :text => " | Home")
+    		                        :text => "#{base_title} | Home")
     end
   end
 
@@ -26,7 +28,7 @@ describe "StaticPages" do
   	it "should have the title 'Help" do
   		visit '/static_pages/help'
   		page.should have_selector('title',
-  			                         :text => " | Help")
+  			                         :text => "#{base_title} | Help")
   	end
   end
 
@@ -40,7 +42,7 @@ describe "StaticPages" do
   	it "should have the title 'About Us'" do
   		visit '/static_pages/about'
   		page.should have_selector('title',
-  			                        :text => " | About Us")
+  			                        :text => "#{base_title} | About Us")
   	end
   end
 end
