0283a9b4f05722363ab4153f0a1f40ff2bc8a4a3
medici <the.medici.florentine@gmail.com>
2013-04-30 13:50:19 +0800

    internationlization with no test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if params[:set_locale]
+      redirect_to root_path(locale: params[:set_locale])
+    end
     if signed_in?
       @microblog = current_user.microblogs.build 
       @feed_items = current_user.feed.paginate(page: params[:page])
