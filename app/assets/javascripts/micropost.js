0ad1988c1c85ff417cff81e28fa0c686646a9bce
medici <the.medici.florentine@gmail.com>
2013-05-21 20:41:23 +0800

    add retweet

--- app/assets/javascripts/micropost.js
+++ app/assets/javascripts/micropost.js
@@ -8,4 +8,17 @@ $(document).ready(function() {
   		reply_form.hide();
   	}
   });
+
+  $('.retweet').click(function() {
+  	var reply_form = $(this).parent().children('.retweet_form');
+  	var user_name = $(this).parent().children('.user').children('a').text();
+  	var content = $(this).parent().children('.content').text();
+  	if(reply_form.is(':hidden')) {
+  		reply_form.show();
+  		reply_form.find('textarea').text(user_name + ': ' + content);
+  	}
+  	else {
+  		reply_form.hide();
+  	}
+  });
 });
